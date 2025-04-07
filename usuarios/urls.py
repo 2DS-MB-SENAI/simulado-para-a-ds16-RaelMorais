850ae516412b18f0194351f80950e2b8735d2f54
Israel <israel@maiil.com>
2025-04-07 11:35:13 -0300

    teste

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,7 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('registro/', view=views.create_user),
-    path('logar/', view=views.login_user),
+    path('login/', view=views.login_user),
     path('read/', view=views.root, name="Read Data"),
 
 ]
